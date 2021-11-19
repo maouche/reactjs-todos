42355bf2085d9e3da0ebe0d227b10b731edb661a
Yacine MAOUCHE <yacine92maouche@gmail.com>
2021-11-19 23:38:55 +0100

    add checkbox done

--- src/components/TodosBox.js
+++ src/components/TodosBox.js
@@ -11,7 +11,7 @@ function TodosBox () {
 	const handleSubmit = function (e) {
 		e.preventDefault();
 		if ( input !== "") {
-			setTodos(todos => [{"text": input}, ...todos])
+			setTodos(todos => [{"text": input, done: false}, ...todos])
 			setInput(input => "")
 		}
 	}
@@ -21,17 +21,35 @@ function TodosBox () {
 		setTodos( todos => todos.filter(todo => todo !== todos[index]) )
 	}
 
+	const changeDone = function (e, index) {
+		e.preventDefault()
+		setTodos(todoss => {
+			var newTodos = []
+			for (var i = 0; i < todoss.length; i++) {
+				if (i === index) {
+					newTodos.push({...todoss[i], done: !todoss[i].done})
+				} else {
+					newTodos.push(todoss[i])
+				}
+			}
+			return newTodos
+		})
+	}
+
 	return (
 		<div className="box">
 			<h1 className="title">{todos.length} todo(s) left</h1>
 			<form className="form" onSubmit={(e) => handleSubmit(e)}>
-				<input className="input" type="text" value={input} onChange={(e) => handleChange(e)} />
-				<button type="submit" className="btn" onClick={handleSubmit}>Add</button>
+				<input className="input" type="text" value={input} onChange={(e) => handleChange(e)} placeholder="What needs to be done ?" />
 			</form>
 			<ul className="list">
 				{todos.map((todo, index) =>
 					<li className="item" key={index}>
-						<span className="text">{todo.text}</span>
+						<input className="checkbox" id={"checkbox" + index} type="checkbox" onClick={(e) => changeDone(e, index)} />
+						<label htmlFor={"checkbox" + index} className={"text" + (todo.done ? " active" : "")}>
+							<span className={"boxx" + (todo.done ? " checked" : "")}></span>
+							{todo.text}
+						</label>
 						<button className="btn danger" onClick={(e) => handleRemove(e, index)}>Delete</button>
 					</li>)}
 			</ul>
